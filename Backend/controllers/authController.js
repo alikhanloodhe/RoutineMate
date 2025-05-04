72b7f0bb40033bc242c3dfd6f519449effb1c668
Ali Khan Lodhi <khanalilodhi786@gmail.com>
2025-05-04 19:34:53 +0500

    bcryptjs added

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,5 @@
 import pool from '../config/db.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 // Excluding verify e-mail everything is same like I have
 export const signUp = async (req, res) => {
