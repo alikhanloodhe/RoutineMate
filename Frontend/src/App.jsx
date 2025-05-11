1c41142ae337aa392a31954a03fcb39946699acd
Ali Khan Lodhi <khanalilodhi786@gmail.com>
2025-05-12 01:38:21 +0500

    Added Missing imports

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ToastProvider } from './context/ToastContext';
 import { AuthProvider, useAuth } from './context/AuthContext';
