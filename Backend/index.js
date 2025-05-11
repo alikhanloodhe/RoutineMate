4d97eaa9b34a2242803c4e8bdc4c6c2dc9ad0eb4
Ali Khan Lodhi <khanalilodhi786@gmail.com>
2025-05-11 23:28:30 +0500

    added configuration for deployement

--- Backend/index.js
+++ Backend/index.js
@@ -89,10 +89,14 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT =  3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+//   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
+// });
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
 });
+
 
 export default app;
